1f143951444ef5ef17baf8c335f5a1540b72f1c1
Eitan Bershtein <Eitan.bersh@gmail.com>
2023-04-23 11:18:14 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-from flask import Flask
-app = Flask(__name__)   
+from flask import Flask, jsonify
+
+app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return "Hello World!\n"
+    return jsonify({'message': 'Hello World!'})
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
